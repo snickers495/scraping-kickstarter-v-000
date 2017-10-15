cfde8f63dbda81a6917f917be79ceced84de6b18
Lisa Lee <snickers495@gmail.com>
2017-10-15 17:09:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
 # require libraries/modules here
-
+require 'nokogiri'
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  array = kickstarter.css("li.project.grid_4")
+  p
+  array.each do |post|
+
+
 end
